8054fe3f9df31ac9fe58603100bd0ddfc1cdf78c
Himel Patel <Himel55@gmail.com>
2017-09-05 22:30:02 +1200

    Finish timeframeConverter

--- source/converters/timeframeConverter.py
+++ source/converters/timeframeConverter.py
@@ -1,7 +1,38 @@
+import os, csv
+
 class TimeFrameConverter:
     """docstring for TimeFrame."""
     def __init__(self, inputFile, timeFrame, outputFile):
         self.inputFile = inputFile
         self.timeFrame = timeFrame
         self.outputFile = outputFile
-        print (self.inputFile + " " + self.timeFrame + " " + self.outputFile)
+
+    def generate(self):
+        with open(self.inputFile, newline='') as csvfile:
+            out = open(self.outputFile, "w")
+            reader = csv.reader(csvfile, delimiter=';')
+            i = 0
+            rows = []
+            for row in reader:
+                rows.append(row)
+                i += 1
+                if i == int(self.timeFrame):
+                    new = self.__getNewValue(rows)
+                    out.write(new + "\n")
+                    rows.clear()
+                    i = 0
+            out.close()
+
+    def __getNewValue(self, rows):
+        #set datetime and open
+        new = rows[0][0] + ';' + rows[0][1]
+        high = float(rows[0][2])
+        low = float(rows[0][3])
+        for row in rows:
+            if float(row[2]) > high:
+                high = float(row[2])
+            if float(row[3]) < low:
+                low = float(row[3])
+
+        new += ";{:.6f};{:.6f};".format(high,low) + rows[-1][4]
+        return new
--- tests/Test_timeframeConverter.py
+++ tests/Test_timeframeConverter.py
@@ -1,12 +1,39 @@
 from source.converters.timeframeConverter import TimeFrameConverter
-import unittest
+import unittest, os
 
 class TestTimeFrameConverter(unittest.TestCase):
-    def test_init(self):
-        inputPath = "resources/TestData.csv"
+    def setUp(self):
+        pre = os.path.dirname(__file__)
+        inputPath = pre + "/resources/TestData.csv"
         timeframe = "5"
+        self.outputPath = pre + "/result.csv"
+        self.con = TimeFrameConverter(inputPath, timeframe, self.outputPath)
+
+    def tearDown(self):
+        if os.path.exists(self.outputPath):
+            os.remove(self.outputPath)
+
+    def test_init(self):
+        inputPath = "Test/inputpath.csv"
+        timeframe = "15"
         outputPath = "output.csv"
         con = TimeFrameConverter(inputPath, timeframe, outputPath)
         self.assertEqual(inputPath, con.inputFile)
         self.assertEqual(timeframe, con.timeFrame)
         self.assertEqual(outputPath, con.outputFile)
+
+    def test_generate(self):
+        self.con.generate()
+        expected = "20170601 000000;1.100000;1.125000;1.099900;1.110000"
+        expected2 = "20170601 000500;1.100000;1.120000;1.099900;1.110000"
+        expected3 = "20170601 001000;1.100000;1.125000;1.000000;1.110000"
+        result = open(self.outputPath)
+        line = result.readline().strip()
+        line2 = result.readline().strip()
+        line3 = result.readline().strip()
+        line4 = result.readline().strip()
+        result.close()
+        self.assertEqual("", line4)
+        self.assertEqual(expected3, line3)
+        self.assertEqual(expected2, line2)
+        self.assertEqual(expected, line)
